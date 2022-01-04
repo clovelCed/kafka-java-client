e63bb1af20701bee0494218d48e8db8f8355fe80
cclovel <cclovel@sqli.com>
2022-01-04 16:09:24 +0100

    init simple consumer

--- consumer/src/main/java/fr/viteducode/kafkaclient/consumer/simple/SimpleKafkaConsumerApplication.java
+++ consumer/src/main/java/fr/viteducode/kafkaclient/consumer/simple/SimpleKafkaConsumerApplication.java
@@ -1,9 +1,38 @@
 package fr.viteducode.kafkaclient.consumer.simple;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class SimpleKafkaConsumerApplication {
 
     public static void main(String[] args) {
 
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer1");
 
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singleton("simple_topic"));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Message key : %s - Message value : %s - Message offset : %d - Message partition : %d\n",
+                            record.key(), record.value(), record.offset(), record.partition());
+                }
+            }
+        } finally {
+            kafkaConsumer.close();
+        }
     }
 }
