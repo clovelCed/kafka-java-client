9bbcca3345aae65e1ece37ade5a1743234feca50
cclovel <cclovel@sqli.com>
2022-01-16 21:31:35 +0100

    update kafka consumer

--- consumer/src/main/java/fr/viteducode/kafkaclient/consumer/confluentavrodeserializer/AvroKafkaConsumerApplication.java
+++ consumer/src/main/java/fr/viteducode/kafkaclient/consumer/confluentavrodeserializer/AvroKafkaConsumerApplication.java
@@ -1,7 +1,7 @@
 package fr.viteducode.kafkaclient.consumer.confluentavrodeserializer;
 
-import fr.viteducode.avro.PlayerKey;
-import fr.viteducode.avro.PlayerValue;
+import fr.viteducode.avro.ChampionKey;
+import fr.viteducode.avro.ChampionValue;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -10,7 +10,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class AvroKafkaConsumerApplication {
@@ -25,19 +25,20 @@ public class AvroKafkaConsumerApplication {
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer1");
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        KafkaConsumer<PlayerKey, PlayerValue> kafkaConsumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<ChampionKey, ChampionValue> kafkaConsumer = new KafkaConsumer<>(properties);
 
-        kafkaConsumer.subscribe(Arrays.asList("player_avro", "champion_avro"));
+        kafkaConsumer.subscribe(Collections.singleton("champion_avro"));
 
         try {
 
             while (true) {
 
-                ConsumerRecords<PlayerKey, PlayerValue> records = kafkaConsumer.poll(Duration.ofMillis(100));
+                ConsumerRecords<ChampionKey, ChampionValue> records = kafkaConsumer.poll(Duration.ofMillis(100));
 
-                for (ConsumerRecord<PlayerKey, PlayerValue> record : records) {
-                    System.out.printf("Player id : %s - Player name : %s - Player is playing : %s", record.key().getId(), record.value().getName(), record.value().getIsPlaying());
+                for (ConsumerRecord<ChampionKey, ChampionValue> record : records) {
+                    System.out.printf("Champion id : %s - Champion name : %s", record.key().getId(), record.value().getName());
                 }
                 kafkaConsumer.commitAsync();
             }
